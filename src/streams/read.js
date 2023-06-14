b790775e7c938908c7bf91d7c9ecb764d53416bc
BrBrov <epoc@list.ru>
2023-06-14 23:13:16 +0300

    feat: create stream read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import * as fs from 'node:fs/promises';
+
+const filePath = 'src/streams/files/fileToRead.txt';
+
 const read = async () => {
     // Write your code here 
+    const fh = await fs.open(filePath);
+    const stream = fh.createReadStream({encoding: 'utf8'});
+
+    stream.on('data', (data) => {
+        process.stdout.write(data);
+    });
+
+    stream.on('end', () => {
+        process.stdout.write('\n');
+    });
 };
 
 await read();
