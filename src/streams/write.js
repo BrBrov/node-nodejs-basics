11b218c1c26e0ba71f48a515f440d2f49e597688
BrBrov <epoc@list.ru>
2023-06-14 23:42:34 +0300

    feat: create stream write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import * as fs from 'node:fs/promises';
+import readline from 'node:readline/promises';
+
+const fileOut = './src/streams/files/fileToWrite.txt';
+
 const write = async () => {
     // Write your code here 
+
+    const fh = await fs.open(fileOut, 'w');
+
+    const streamW = fh.createWriteStream({encoding: 'utf8'});
+
+    process.stdin.pipe(streamW);
+    
 };
 
 await write();
