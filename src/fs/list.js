12819b286f1bf4d942d0935efee888a38502b7f8
BrBrov <epoc@list.ru>
2023-06-14 19:42:46 +0300

    feat: create list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import * as fs from 'node:fs/promises';
+
+const filePath = 'src/fs/files';
+const {log} = console;
+
 const list = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const dir = await fs.readdir(filePath);
+    
+    dir.forEach((item) => log(item));
+  } catch (e) {
+    const err = new Error();
+    err.message = 'FS operation failed';
+    err.name = 'Error';
+    throw err;
+  }
 };
 
 await list();
