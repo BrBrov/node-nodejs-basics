48c73df8ef7789af8b2924b50c0e3bb20a23e42e
BrBrov <epoc@list.ru>
2023-06-14 19:45:44 +0300

    feat: create args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,27 @@
+import process from 'node:process';
+
+const { log } = console;
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+  const argArray = process.argv.slice(2);
+
+  if (!argArray.length) return;
+
+  const outputString = [];
+
+  let argString = '';
+
+  argArray.forEach((item, index) => {
+    if ((index % 2) == 0) {
+      argString += index == 0 ? `${item.slice(2)} is ` : ` ${item.slice(2)} is `;
+    } else {
+      argString += `${item}`;
+      outputString.push(argString);
+      argString = '';
+    }
+  });
+
+  log(outputString.toString());
 };
 
 parseArgs();
