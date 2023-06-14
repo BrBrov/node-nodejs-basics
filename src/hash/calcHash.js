64d9ec80b983a61594d5b2239bdfa47442fc1f36
BrBrov <epoc@list.ru>
2023-06-14 22:38:25 +0300

    feat: create hash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import * as fs from 'node:fs/promises';
+import crypto from 'node:crypto';
+
+const filePath = 'src/hash/files/fileToCalculateHashFor.txt';
+const algo = 'sha256';
+
 const calculateHash = async () => {
     // Write your code here 
+   const fileData = await fs.readFile(filePath, {encoding: 'utf-8'});
+   const buffer = fileData.toString('hex');
+   const sha = crypto.createHash(algo).update(buffer).digest('hex');
+   console.log(sha);
 };
 
 await calculateHash();
