102e58a572870d17a5b8d4dc768b39233ead3bb3
BrBrov <epoc@list.ru>
2023-06-14 18:49:22 +0300

    feat: create copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+
+const folderPath = 'src/fs/files';
+const destinationFolder = 'src/fs/files_copy';
+
 const copy = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const dir = await fs.readdir(folderPath);
+
+    await fs.mkdir(destinationFolder);
+
+    dir.forEach((file) => {
+      const pathFile = path.join(folderPath, file);
+      const pathNewFile = path.join(destinationFolder, file);
+      fs.copyFile(pathFile, pathNewFile);
+    });
+  } catch (e) {
+    const err = new Error(`FS operation failed`);
+    err.name = 'Error';
+    throw err;
+  }
 };
 
 await copy();
