a2de5c02de651c46c778321f56ce2f9ff002da5f
BrBrov <epoc@list.ru>
2023-06-14 18:58:00 +0300

    feat: create delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+
+const fileName = 'fileToRemove.txt';
+const pathFile = path.join('src/fs/files', fileName);
+
 const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await fs.rm(pathFile);
+  } catch (e) {
+    const err = new Error(`FS operation failed`);
+    err.name = 'Error';
+    throw err;
+  }
 };
 
 await remove();
