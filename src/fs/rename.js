761d7167bf6d939219c720b88f16b8e6e4dbe326
BrBrov <epoc@list.ru>
2023-06-14 18:52:22 +0300

    feat: create rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+
+const pathFolder = 'src/fs/files';
+const oldName = 'wrongFilename.txt';
+const newName = 'properFilename.md';
+
 const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    const result = await fs.rename( path.join(pathFolder, oldName), path.join(pathFolder, newName));
+  } catch (e) {
+    const err = new Error(`FS operation failed`);
+    err.name = 'Error';
+    throw err;
+  }
 };
 
 await rename();
