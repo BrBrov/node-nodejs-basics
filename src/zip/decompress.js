8c3c3fff0fbbc006196c6f1b6de1c672b5fa2ada
BrBrov <epoc@list.ru>
2023-06-15 16:28:54 +0300

    feat: crete compress and decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import * as fs from 'node:fs/promises';
+import zlib from 'node:zlib';
+
+const fileName = 'fileToCompress.txt';
+const filePath = 'src/zip/files/';
+const fileZip = 'archive.gz';
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+    const fZip = await fs.open(filePath + fileZip, 'r');
+    const fZipStream = fZip.createReadStream();
+
+    await fs.appendFile(filePath + fileName, '', {encoding: 'utf8'});
+
+    const fh = await fs.open(filePath + fileName, 'w');
+    const fStream = fh.createWriteStream({encoding: 'utf-8'});
+
+    const gUnZip = zlib.createGunzip();
+
+    fZipStream.pipe(gUnZip).pipe(fStream);
 };
 
 await decompress();
