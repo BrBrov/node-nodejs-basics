f044f02070e1702afa7c5937312c9a618bdd3d1d
BrBrov <epoc@list.ru>
2023-06-16 19:14:53 +0300

    feat: create worker task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,43 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
+const { log } = console;
+
+function WorkerResultObject(status, value) {
+    this.status = status;
+    this.data = value;
+}
+
+function createWorker(counter) {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker('./src/wt/worker.js', { workerData: counter });
+
+        worker.on('message', (value) => resolve(new WorkerResultObject('resolved', value)));
+
+        worker.on('error', (e) => reject(new WorkerResultObject('error', null)));
+    });
+}
+
+async function* workerGenerator(cpuCount) {
+    let startCPUCounter = 10;
+    for (let index = 0; index < cpuCount; index++) {
+        yield await createWorker(startCPUCounter);
+        startCPUCounter += 1;
+    }
+}
+
 const performCalculations = async () => {
     // Write your code here
+    const cpusCount = os.cpus().length;
+    const resultArray = [];
+
+    const workerObject = workerGenerator(cpusCount);
+
+    for await (const value of workerObject) {
+        resultArray.push(value);
+    }
+
+    log(resultArray);
 };
 
 await performCalculations();
